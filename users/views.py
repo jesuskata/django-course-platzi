fb74b0d7560cd8aac00774cb087c0e0c161a1ff7
jesuskata <rzarate.alex@gmail.com>
2019-10-25 12:27:06 -0500

    Created and configured the views for the users login.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+"""Users views"""
 
-# Create your views here.
+# Django
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+def login_view(request):
+    """Login view"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('feed') # feed es el nombre que le pusimos a la url de feed
+        else:
+            return render(request, 'users/login.html', {'error': 'Invalid username and/or password'})
+    return render(request, 'users/login.html')
