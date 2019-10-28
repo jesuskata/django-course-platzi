69a1d63195dc4bfa736b4301d23f0e6b661231a4
jesuskata <rzarate.alex@gmail.com>
2019-10-28 13:03:51 -0600

    Added the logout feature.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 """Users views"""
 
 # Django
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 def login_view(request):
@@ -17,3 +18,9 @@ def login_view(request):
         else:
             return render(request, 'users/login.html', {'error': 'Invalid username and/or password'})
     return render(request, 'users/login.html')
+
+@login_required
+def logout_view(request):
+    """Logout a user"""
+    logout(request)
+    return redirect('login')
